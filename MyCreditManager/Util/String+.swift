7b0996a5d73fd4f397475eb38c62c166a4ea78f9
Sonmoham <dnddl505@gmail.com>
2022-11-17 18:21:02 +0900

    [Fix] 유효성 검사 regex 오류 수정

--- MyCreditManager/Util/String+.swift
+++ MyCreditManager/Util/String+.swift
@@ -9,7 +9,7 @@ import Foundation
 
 extension String {
     func isValid() -> Bool {
-        let regex = "^[ 0-9a-zA-Z]"     // 스페이스, 숫자, 영문 대소
+        let regex = "^[ 0-9a-zA-Z]*$"     // 스페이스, 숫자, 영문 대소
         let test = NSPredicate(format: "SELF MATCHES %@", regex)
         return test.evaluate(with: self)
     }
