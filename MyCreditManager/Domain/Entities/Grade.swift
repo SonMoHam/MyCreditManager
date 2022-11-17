d1dc3f93962e98183ff04e26a8fb1974e53e844d
Sonmoham <dnddl505@gmail.com>
2022-11-17 12:03:04 +0900

    [Feat] 도메인 엔티티 Grade 구현

--- MyCreditManager/Domain/Entities/Grade.swift
+++ MyCreditManager/Domain/Entities/Grade.swift
@@ -6,3 +6,47 @@
 //
 
 import Foundation
+
+enum Grade: String, CustomStringConvertible {
+    case aP
+    case a
+    case bP
+    case b
+    case cP
+    case c
+    case dP
+    case d
+    case f
+    
+    init?(_ input: String) {
+        let str = input.lowercased().replacingOccurrences(of: "+", with: "P")
+        self.init(rawValue: str)
+    }
+    
+    var description: String {
+        return self.rawValue.uppercased().replacingOccurrences(of: "P", with: "+")
+    }
+    
+    var score: Double {
+        switch self {
+        case .aP:
+            return 4.5
+        case .a:
+            return 4
+        case .bP:
+            return 3.5
+        case .b:
+            return 3
+        case .cP:
+            return 2.5
+        case .c:
+            return 2
+        case .dP:
+            return 1.5
+        case .d:
+            return 1
+        case .f:
+            return 0
+        }
+    }
+}
