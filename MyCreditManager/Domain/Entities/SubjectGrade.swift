7a20c6772af9e3831eb1438761202bab6192454d
Sonmoham <dnddl505@gmail.com>
2022-11-17 16:23:35 +0900

    [Feat] 도메인 엔티티 과목성적 SubjectGrade 추가

---
+++ MyCreditManager/Domain/Entities/SubjectGrade.swift
@@ -0,0 +1,14 @@
+//
+//  SubjectGrade.swift
+//  MyCreditManager
+//
+//  Created by Son Daehong on 2022/11/17.
+//
+
+import Foundation
+
+struct SubjectGrade {
+    let student: Student
+    let subject: Subject
+    let grade: Grade
+}
