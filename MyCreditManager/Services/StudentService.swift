b8bd38a26abb5955eb250cc900cc99fc5a1d0819
Sonmoham <dnddl505@gmail.com>
2022-11-17 18:23:09 +0900

    [Feat] StudentService post, delete 구현

--- MyCreditManager/Services/StudentService.swift
+++ MyCreditManager/Services/StudentService.swift
@@ -15,10 +15,40 @@ final class StudentService {
     }
     
     func post() {
-        
+        let guide = "추가할 학생의 이름을 입력해주세요"
+        let name = getStudentName(inputGuide: guide)
+        let student = Student(name: name)
+        let result = studentUseCase.insert(student)
+        switch result {
+        case .success:
+            print("\(name) 학생을 추가했습니다.")
+        case .failure(let error):
+            print(error.localizedDescription)
+        }
     }
     
     func delete() {
-        
+        let guide = "삭제할 학생의 이름을 입력해주세요"
+        let name = getStudentName(inputGuide: guide)
+        let result = studentUseCase.delete(name: name)
+        switch result {
+        case .success:
+            print("\(name) 학생을 삭제하였습니다.")
+        case .failure(let error):
+            print(error.localizedDescription)
+        }
+    }
+    
+    
+    private func getStudentName(inputGuide: String) -> String {
+        while true {
+            print(inputGuide)
+            if let input = readLine(), input.isValid() {
+                return input.capitalized    // 대소문자 형식 고정
+            } else {
+                print("입력이 잘못되었습니다. 다시 확인해주세요.")
+                continue
+            }
+        }
     }
 }
