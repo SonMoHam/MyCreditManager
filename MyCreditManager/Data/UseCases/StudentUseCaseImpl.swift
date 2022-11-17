b6c8bec98f9a109c174d7df83e5e71327a87a3b7
Sonmoham <dnddl505@gmail.com>
2022-11-17 17:38:10 +0900

    [Feat] Data usecases - StudentUseCase 구현체 Stub, Default 추가

--- MyCreditManager/Data/UseCases/StudentUseCaseImpl.swift
+++ MyCreditManager/Data/UseCases/StudentUseCaseImpl.swift
@@ -8,4 +8,19 @@
 import Foundation
 
 
-public final class StubStudentUseCase { }
+public final class StubStudentUseCase: StudentUseCase {
+    public func get(name: String) -> Result<Student, Error> {
+        let dummyStd = Student(name: name)
+        return .success(dummyStd)
+    }
+    
+    public func insert(_ student: Student) -> Result<Void, Error> {
+        return .success
+    }
+    
+    public func delete(name: String) -> Result<Void, Error> {
+        return .success
+    }
+}
+
+public final class DefaultStudentUseCase { }
