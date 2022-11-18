b57048f77bbf1d2636494948a9b229370cc761cb
Sonmoham <dnddl505@gmail.com>
2022-11-18 16:09:23 +0900

    [Feat] StudentsStorage 구현체 - UserDefautlsStudentsStorage 구현

---
+++ MyCreditManager/Data/Storages/StudentsStorage/UserDefaultsStudentsStorage.swift
@@ -0,0 +1,73 @@
+//
+//  UserDefaultsStudentsStorage.swift
+//  MyCreditManager
+//
+//  Created by Son Daehong on 2022/11/18.
+//
+
+import Foundation
+
+enum stubError: Error {
+    case stub
+}
+
+final class UserDefaultsStudentsStorage{
+    private let encoder: JSONEncoder
+    private let decoder: JSONDecoder
+    
+    init() {
+        encoder = JSONEncoder()
+        decoder = JSONDecoder()
+    }
+    
+    private func search(_ name: String) -> Student? {
+        let students = fetchStudents()
+        return students.filter { $0.name == name }.first
+    }
+    
+    private func fetchStudents() -> [Student] {
+        guard let data = UserDefaults.StudentManage.object(forKey: .students) as? Data,
+              let students = try? decoder.decode([Student].self, from: data)
+        else { return [] }
+        return students
+    }
+    
+    private func update(_ students: [Student]) {
+        let encoded = try? encoder.encode(students)
+        UserDefaults.StudentManage.set(value: encoded, forKey: .students)
+    }
+}
+
+extension UserDefaultsStudentsStorage: StudentsStorage {
+    func create(_ student: Student) -> Result<Void, Error> {
+        let students = fetchStudents()
+        if students.contains(student) {
+            // TODO: return error: student.name은 이미 존재하는 학생입니다. 추가하지 않습니다
+            return .success
+        } else {
+            var newStudents = students
+            newStudents.append(student)
+            update(newStudents)
+            return .success
+        }
+    }
+    
+    func read(_ name: String) -> Result<Student, Error> {
+        guard let target = search(name) else {
+            // TODO: return error: student.name 학생을 찾지 못했습니다.
+            return .failure(stubError.stub)
+        }
+        return .success(target)
+    }
+    
+    func delete(_ name: String) -> Result<Void, Error> {
+        guard let target = search(name) else {
+            // TODO: return error: student.name 학생을 찾지 못했습니다.
+            return .failure(stubError.stub)
+        }
+        let students = fetchStudents()
+        let newStudents = students.filter { $0 != target }
+        update(newStudents)
+        return .success
+    }
+}
