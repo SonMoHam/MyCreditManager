62be7a172db7f3dd65548961d469a677d3c88f7b
Sonmoham <dnddl505@gmail.com>
2022-11-18 10:35:30 +0900

    [Fix] GradeService - put query 입력 시 코드 수정

--- MyCreditManager/Services/GradeService.swift
+++ MyCreditManager/Services/GradeService.swift
@@ -15,7 +15,7 @@ final class GradeService {
     }
     
     func put() {
-        let subjectGrade = getPutQuery()
+        guard let subjectGrade = getPutQuery() else { return }
         let result = subjectGradeUseCase.put(subjectGrade)
         switch result {
         case .success:
@@ -42,26 +42,25 @@ final class GradeService {
         
     }
     
-    private func getPutQuery() -> SubjectGrade {
+    private func getPutQuery() -> SubjectGrade? {
         let countOfQueryParams: Int = 3
         let guide = "성적을 추가할 학생의 이름, 과목 이름, 성적(A+, A, F 등)을 띄어쓰기로 구분하여 차례로 작성해주세요.\n입력예) Mickey Swift A+\n만약에 학생의 성적 중 해당 과목이 존재하면 기존 점수가 갱신됩니다."
-        while true {
-            print(guide)
-            guard let input = readLine(), validate(input, countOfQueryParams: countOfQueryParams)
-            else {
-                showInputInvalid()
-                continue
-            }
-            let params = input.split(separator: " ").map { String($0) }
-            guard let grade = Grade(rawValue: params[2]) else {
-                showInputInvalid()
-                continue
-            }
-            return SubjectGrade(
-                student: Student(name: params[0]),
-                subject: Subject(name: params[1]),
-                grade: grade)
+
+        print(guide)
+        guard let input = readLine(), validate(input, countOfQueryParams: countOfQueryParams)
+        else {
+            showInputInvalid()
+            return nil
         }
+        let params = input.split(separator: " ").map { String($0) }
+        guard let grade = Grade(rawValue: params[2]) else {
+            showInputInvalid()
+            return nil
+        }
+        return SubjectGrade(
+            student: Student(name: params[0]),
+            subject: Subject(name: params[1]),
+            grade: grade)
     }
     
     private func getDeleteQuery() -> (studentName: String, subjectName: String)? {
